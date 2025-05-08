180fe18ed27ef64652a58e5c01c41b46f69bb0ce
Bobby Dahal <IAMBOBBYDAHAL@GMAIL.COM>
2025-05-08 01:37:11 +0000

    Variables declaration done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1318 //Use Const
+let accountName = "Bobby Dahal"// Use let
+var accountPass = "Bobby@1318" //Ignore var as much as possible it affects the scope of the program
+accountCity = "Pokhara" //Not GOOD WAY
+let accountAge;
+
+accountName = "Hero"
+accountCity = "Varanasi"
+accountPass = "Nor19"
+console.log(accountId);
+
+console.table([accountId,accountName,accountPass,accountCity,accountAge])
