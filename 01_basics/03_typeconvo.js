de553e8c1367e6c1a6734221542196d896055056
Bobby Dahal <IAMBOBBYDAHAL@GMAIL.COM>
2025-05-08 05:53:40 +0000

    basic of type conversion done

---
+++ 01_basics/03_typeconvo.js
@@ -0,0 +1,16 @@
+let score =  "33Qaa"
+let numScore = Number(score)
+console.log(typeof(score))
+console.log(typeof(numScore))
+console.log(numScore)
+
+let isNum = 1
+let boolNum = Boolean(isNum)
+console.log(boolNum);
+let stringNum = String(boolNum)
+console.log(stringNum);
+console.log(String(isNum))
+console.log(typeof String(isNum));
+
+
+
