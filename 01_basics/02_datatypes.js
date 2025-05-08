a4c9a83a1937a8fb39f5f683e3cc4529f26ebcd5
Bobby Dahal <IAMBOBBYDAHAL@GMAIL.COM>
2025-05-08 02:41:42 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version 
+
+const Id = 1318 // number dataType can store vlue upto 2 ^ 53
+let name = "Bobby" // string dataType
+let isActive = true // bool dataType
+let future; // undefined dataType 
+let salary = null ; // Type is given as object
+
+// other important
+//bigint
+//Symbol -> use for uniqueness
+// object
+
+console.log(typeof "Bobby") //String
+console.log(typeof future) // undefined
+console.log(typeof null) // Gives Object as type
