2e2fc765108875962127b8ec69f56a2ec4fb7eaf
Bobby Dahal <IAMBOBBYDAHAL@GMAIL.COM>
2025-05-13 16:12:33 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+// 2 types of Data -> Primitive and Non Primitive Data Type
+
+// Primitive DataType (Call By Value)
+// 7 types ->String, Number, Boolean, Symbol, BigInt, null, undefined
+
+let userName = "Bobby" // type-> string
+let age = 22 // type-> number
+let gpa = 3.7 // type-> number
+let isActive = true // type-> boolean
+let id = Symbol("123") //  type-> symbol
+let anotherID = Symbol("123")
+console.log(id === anotherID); 
+let bigNumber = 3n // type-> bigInt
+let temp = null // type-> object
+let wife; // type-> undefined
+
+
+// Referential type (Non Primitive) -> Call By Reference
+// 3 -> Array, Object, Function
+
+let place = ["kashi","Ramesworam","Ujjain","Omkar"] // type-> object
+let newObj = {
+    name:"Bobby",
+    score:100,
+} // type-> object
+const func = function(){
+    console.log("Hello World!");
+} // type-> function object
+console.log(typeof func);
+
